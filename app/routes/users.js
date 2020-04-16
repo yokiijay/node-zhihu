c9fb1dd5dc0f018bc57d972710e08a58a241ef5f
Yokiijay <yokiijay@gmail.com>
2020-04-16 15:11:59 +0800

    手动编写auth完成

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,14 +3,19 @@ const router = new Router({prefix: '/users'})
 const users = require('../controllers/users')
 const jsonwebtoken = require('jsonwebtoken')
 const { secret } = require('../../config')
+// const jwt = require('koa-jwt')
 
 const auth = async (ctx, next)=>{
+  // 从请求头中获取token
   const { authorization='' } = ctx.request.header
   const token = authorization.replace('Bearer ', '')
   try {
+    // 解析认证token
     const user = jsonwebtoken.verify(token, secret)
+    // 通过 设置用户信息
     ctx.state.user = user
   }catch (err) {
+    // 不通过
     ctx.throw(401, err.message) //未登录
   }
 
