4928053f64b85021ef0d0650e56011cc902c17c6
Yokiijay <yokiijay@gmail.com>
2020-04-18 03:37:16 +0800

    rename

---
+++ app/routes/usersRoute.js
@@ -0,0 +1,29 @@
+const Router = require('koa-router')
+const router = new Router({prefix: '/users'})
+const users = require('../controllers/usersController')
+const { secret } = require('../../config')
+const jwt = require('koa-jwt')
+
+const auth = jwt({secret})
+
+router.get('/', users.getUserByQuery)
+
+router.get('/:id', users.getUserById)
+
+router.patch('/:id', auth, users.checkOwner, users.updateUser)
+
+router.post('/', users.createUser)
+
+router.delete('/:id', auth, users.checkOwner, users.deleteUserById)
+
+router.post('/login', users.login)
+
+router.get('/:id/following', users.getFollowings)
+
+router.get('/:id/followers', users.getFollowers)
+
+router.put('/following/:id', auth, users.follow)
+
+router.delete('/following/:id', auth, users.unfollow)
+
+module.exports = router
